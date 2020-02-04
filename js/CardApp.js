602b60f2c77ec1607863d74a6ec50d8b6e912be7
kanozo12 <kanozo12@naver.com>
2020-02-04 19:38:49 +0900

    20200204

--- js/CardApp.js
+++ js/CardApp.js
@@ -2,69 +2,100 @@ class CardApp {
     constructor() {
         this.cardList = $(".card-list");
         this.word = $("#word");
-        this.addEvent();
+        this.opt = $("#searchOpt")
         this.data = [];
+        this.range = $(".slider");
 
+        $(".register").hide();
         $.ajax({
-            url: '/data.php',
+            url: '/data.json',
             method: 'get',
             success: (data) => {
-                this.data = data;
+                this.data = data;                
+                this.range.attr("max", Math.max(...data.map( x => x.price)));
+                this.range.val(Math.max(...data.map( x => x.price)));
+                this.range.attr("min", Math.min(...data.map( x => x.price)));
+
                 this.loadData(data);
             }
         });
-
+        this.addEvent();
     }
 
-    addEvent() {
-        $("#btnSearch").on("click", this.serach);
+    addEvent(){
+        $("#btnSearch").on("click", this.search);
+        this.range.on("change", this.filter);
+        $("#register").on("click", this.loadRegisterPage);
+        $("#registerOK").on("click", this.registerCard);
+
+        $(document).on("click", ".up", this.uplike);
+        $("#sortData").on("click", ()=>{
+            let sortedData = this.data.sort((a, b) => a.result - b.result);
+            this.loadData(sortedData);
+        });
     }
 
-    serach() {
+    uplike = (e) => {
+        let value = $(e.target).data("result");
+        $(e.target).siblings(".result").html((value + 1) + "명");
+        $(e.target).data("result", value + 1);
+    }
 
-        // console.log(this.search);
+    registerCard = () => {
+        let name = $("#name").val();
+        let price = $("#price").val();
+        let result = $("#result").val();
+
+        this.data.push({image:"busan.jpg", name, price, result});
+        $(".container").show('slow');
+        $(".register").hide('slow');
+        this.loadData(this.data);
+    };
+
+    loadRegisterPage = () => {
+        $(".container").hide('slow');
+        $(".register").show('slow');
+    }
+
+    filter = () => {
+        //여기에 range의 범위안에 있는 값들만을 출력하도록 만들어봐.
+        console.log(this.range.val());
+        let filterData = this.data.filter(x => x.price <= this.range.val())
+            .sort((a, b) => a.price - b.price);
+        this.loadData(filterData);
+    }
+
+    search = () => {
         let word = this.word.val();
-        let regex = new RegExp(word, "gi");
-        let search = $("#searchOpt option:selected").val();
-        if (search == "name") {
-            this.data.forEach(item => {
 
-                let filterData = this.data.filter(item => item.name.includes(word)).map(item => {
-                    let newItem = JSON.parse(JSON.stringify(item));
-                    newItem.name = newItem.name.replace(regex, `<span class="c">$&</span>`);
-                    return newItem;
+        if(this.opt.val() == "name"){
+            let regex = new RegExp(word,"gi");
+            let filterData = this.data.filter(
+                item => item.name.includes(word)).map( x => {
+                    let item = JSON.parse(JSON.stringify(x));
+                    item.name = item.name.replace(regex, `<span class="hl">$&</span>`);
+                    return item;
                 });
-                this.loadData(filterData);
-
+            this.loadData(filterData);
+        }else {
+            let filterData = this.data.filter( x => x.price <= word).map( x => {
+                let item = JSON.parse(JSON.stringify(x));
+                item.price = `<span class="hl">${item.price}</span>`;
+                return item;
             });
-
-        } else if (search == "price") {
-            this.data.forEach(item => {
-
-                let filterData = this.data.filter(item => word >= item.price);
-                console.log(filterData);
-
-                filterData = filterData.map(item => {
-                    let newItem = JSON.parse(JSON.stringify(item));
-                    newItem.price = newItem.price.toString().replace(newItem.price, `<span class="c">$&</span>`);
-                    return newItem;
-                });
-
-                this.loadData(filterData);
-            });
+            this.loadData(filterData);
         }
-
+        
     }
 
     loadData(data) {
         this.cardList.empty();
-        data.forEach((item, idx) => {
+        data.forEach((item, idx) => {            
             let str = this.makeCard(item);
             this.cardList.append(str);
             setTimeout(() => {
                 $(".card-list > .card").eq(idx).addClass("active");
-            }, 50 + idx * 400);
-
+            }, 50 + idx * 100);
         });
     }
 
@@ -79,8 +110,10 @@ class CardApp {
                 <h4 class="price">${item.price}원</h4>
                 <div class="grade">
                     <span class="result">${item.result}명</span>
+                    <button class="up" data-result="${item.result}">좋아요</button>
                 </div>
             </div>
-        </div>`;
+        </div>
+        `;
     }
 }
